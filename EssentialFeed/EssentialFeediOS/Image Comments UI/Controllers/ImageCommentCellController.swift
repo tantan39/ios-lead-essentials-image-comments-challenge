800a69bd2d603e32d61eb2efd0dd18d2569da235
Tan Tran <tantan39>
2021-08-18 14:35:25 -0400

    Add snapshot tests

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,26 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class ImageCommentCellController: NSObject, UITableViewDataSource {
+	let model: ImageCommentViewModel
+
+	public init(model: ImageCommentViewModel) {
+		self.model = model
+	}
+
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell: ImageCommentCell = tableView.dequeueReusableCell()
+		cell.usernameLabel.text = model.username
+		cell.dateLabel.text = model.date
+		cell.messageLabel.text = model.message
+		return cell
+	}
+}
---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class ImageCommentCell: UITableViewCell {
+	@IBOutlet public private(set) var messageLabel: UILabel!
+	@IBOutlet public private(set) var usernameLabel: UILabel!
+	@IBOutlet public private(set) var dateLabel: UILabel!
+}
