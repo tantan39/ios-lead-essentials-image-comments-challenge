fb2633e1c5dd251fcb857d50e2d50476dabe7b16
Tan Tran <tantan39>
2021-08-30 09:31:54 -0400

    Avoid extra logic by hardcoding the imageID instead of a UUID type

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -11,7 +11,7 @@ class ImageCommentsEndpointTests: XCTestCase {
 		let baseURL = URL(string: "http://base-url.com")!
 
 		let received = ImageCommentsEndpoint.get(imageID).url(baseURL: baseURL)
-		let expected = URL(string: "http://base-url.com/v1/image/\(imageID)/comments")
+		let expected = URL(string: "http://base-url.com/v1/image/2239CBA2-CB35-4392-ADC0-24A37D38E010/comments")
 
 		XCTAssertEqual(received, expected)
 	}
